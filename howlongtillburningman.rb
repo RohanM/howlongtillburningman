634e87aa1620a901b6b71014e78eedcda3f07fd1
Rohan Mitchell <rohan@rohanmitchell.com>
2011-11-08 15:30:30 +1100

    Add date calculations and countdown till the man burn.

--- howlongtillburningman.rb
+++ howlongtillburningman.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'date'
+
+DATES = {:burn => Date.new(2012, 9, 1),
+         :gate => Date.new(2012, 8, 27),
+         :tickets => Date.new(2012, 1, 18)}
 
 get '/' do
+  @days_till = DATES.merge(DATES) { |event, date| (date - Date.today).to_i }
+
   haml :index
 end
