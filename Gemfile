44e49397dc8f7402c89cd1e893ad179b61577f47
Rohan Mitchell <rohan@rohanmitchell.com>
2011-12-10 13:52:05 +1100

    Display the next forthcoming ticket lottery date

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source :rubygems
 
 gem 'sinatra'
+gem 'actionpack'
 gem 'haml'
--- howlongtillburningman.rb
+++ howlongtillburningman.rb
@@ -1,12 +1,27 @@
 require 'sinatra'
 require 'date'
+require 'action_view'
+include ActionView::Helpers::TextHelper
 
-DATES = {:burn => Date.new(2012, 9, 1),
-         :gate => Date.new(2012, 8, 27),
-         :tickets => Date.new(2012, 1, 18)}
+EVENT_DATES = [{:message => "The man burns in", :date => Date.new(2012, 9, 1)},
+               {:message => "The gate opens in", :date => Date.new(2012, 8, 27)}]
+
+TICKET_DATES = [{:message => "The pre-sale ticket round closes in", :date => Date.new(2011, 12, 11)},
+                {:message => "The pre-sale ticket lottery is drawn in", :date => Date.new(2011, 12, 19)},
+                {:message => "The main sale ticket round opens in", :date => Date.new(2012, 1, 9)},
+                {:message => "The main sale ticket round closes in", :date => Date.new(2012, 1, 22)},
+                {:message => "The main-sale ticket lottery is drawn in", :date => Date.new(2012, 2, 1)},
+                {:message => "The secondary ticket sale round opens in", :date => Date.new(2012, 3, 1)}]
 
 get '/' do
-  @days_till = DATES.merge(DATES) { |event, date| (date - Date.today).to_i }
+  @dates = EVENT_DATES.map { |date| {:message => date[:message], :days_till => (date[:date] - Date.today).to_i } }
+
+  TICKET_DATES.each do |date|
+    if date[:date] > Date.today
+      @dates << {:message => date[:message], :days_till => (date[:date] - Date.today).to_i}
+      break
+    end
+  end
 
   haml :index
 end
